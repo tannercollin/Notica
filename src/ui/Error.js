a724e99bfc44c801add45f6e8fb51bf9bef74ede
Tanner Collin <git@tannercollin.com>
2016-12-30 07:52:34 +0000

    Complete error page

--- src/ui/Error.js
+++ src/ui/Error.js
@@ -4,8 +4,18 @@ import React from 'react';
 export default class Error extends React.Component {
 	render(){
 		return (
-			<div>
-				Error page.
+			<div className="container">
+				<div className="row">
+					<div className="twelve columns">
+						<h4>Error</h4>
+						<p>
+							Something went wrong so we stopped everything and went fishing.
+						</p>
+						<video className="u-max-full-width" poster="https://i.imgur.com/vfcuwyah.jpg" preload="auto" autoPlay={true} muted="muted" loop="loop" webkit-playsinline="">
+							<source src="https://i.imgur.com/vfcuwya.mp4" type="video/mp4" />
+						</video>
+					</div>
+				</div>
 			</div>
 		);
 	}
